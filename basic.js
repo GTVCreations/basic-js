82e21fb6abc74670111d327a778334575ac83963
Thiruvikraman Govindarajan <gtvcreations@gmail.com>
2019-11-03 15:24:24 +0530

    URLFriendly is Enabled with Numbers

--- basic.js
+++ basic.js
@@ -1,10 +1,10 @@
-/*! basic.js v0.4.2 | MIT License | Copyright (c) 2019 Thiruvikraman Govindarajan | github.com/gtvcreations/basic.js */
+/*! basic.js v0.4.3 | MIT License | Copyright (c) 2019 Thiruvikraman Govindarajan | github.com/gtvcreations/basic.js */
 
 // Strict Mode
 "use strict";
 
 // Universal JS Module Loader
-;(function(root, factory) {
+; (function (root, factory) {
 
     if (typeof define === "function" && define.amd) {
         define(factory);
@@ -14,13 +14,13 @@
         root.Basic = factory();
     }
 
-})(this, function() {
-    
+})(this, function () {
+
     // Object
     var Basic = {};
 
     // Version
-    Basic.version = "0.4.2";
+    Basic.version = "0.4.3";
 
     // Where Am I
     Basic.isBrowser = (typeof window === "object") ? true : false;
@@ -28,45 +28,45 @@
     Basic.isNode = (typeof process === "object") ? true : false;
 
     // Check the Input is Array
-    Basic.isArray = function(aInputArr) {
+    Basic.isArray = function (aInputArr) {
         var inputArr = aInputArr;
         return (inputArr instanceof Array) ? true : false;
     };
 
     // Sort Array of Numbers in Ascending Order
-    Basic.sortNum = function(aArrOfNum) {
+    Basic.sortNum = function (aArrOfNum) {
         var numArr = aArrOfNum;
         // concat() return new array. It doesn't affect existing array
-		return numArr.concat().sort(function(a, b) {return a-b});
+        return numArr.concat().sort(function (a, b) { return a - b });
     };
 
     // Sort Array of Numbers in Descending Order
-    Basic.rsortNum = function(aArrOfNum) {
+    Basic.rsortNum = function (aArrOfNum) {
         var numArr = aArrOfNum;
         // concat() return new array. It doesn't affect existing array
-        return numArr.concat().sort(function(a, b) {return b-a});
+        return numArr.concat().sort(function (a, b) { return b - a });
     };
 
     // Remove Duplicate Entries in Array
-    Basic.uniqueArr = function(aArrOfAny) {
+    Basic.uniqueArr = function (aArrOfAny) {
         var anyArr = aArrOfAny;
-        return anyArr.filter(function(item, pos) {
+        return anyArr.filter(function (item, pos) {
             return anyArr.indexOf(item) == pos;
         });
     };
 
     // Remove Particular Values in Array
-    Basic.delArrVal = function(aArrOfAny, aRmValArrOfAny) {
+    Basic.delArrVal = function (aArrOfAny, aRmValArrOfAny) {
         var anyArr = aArrOfAny;
         var remArr = aRmValArrOfAny;
 
         anyArr = anyArr.concat();
 
-        for(var i = 0; i < remArr.length; i++) {
-            for(var j = 0; j < anyArr.length; j++) {
-                if(anyArr[j] === remArr[i]) {
+        for (var i = 0; i < remArr.length; i++) {
+            for (var j = 0; j < anyArr.length; j++) {
+                if (anyArr[j] === remArr[i]) {
                     anyArr.splice(j, 1);
-                    j = j-1;
+                    j = j - 1;
                 }
             }
         }
@@ -75,88 +75,89 @@
     };
 
     // Add / Sum of `n` Numbers from Array
-    Basic.sum = function(aArrOfNum) {
+    Basic.sum = function (aArrOfNum) {
         var sum = 0;
-        for(var i = 0; i < aArrOfNum.length; i++) {
+        for (var i = 0; i < aArrOfNum.length; i++) {
             sum += aArrOfNum[i];
         }
         return sum;
     };
 
     // Mean / Average / Calculate Central Value of `n` Numbers from Array
-    Basic.mean = function(aArrOfNum) {
+    Basic.mean = function (aArrOfNum) {
         var numArr = aArrOfNum;
         return (aArrOfNum != 0) ? (Basic.sum(numArr) / numArr.length) : numArr.length;
     };
 
     // Median
-    Basic.median = function(aArrOfNum) {
+    Basic.median = function (aArrOfNum) {
         var numArr = aArrOfNum;
         numArr = Basic.sortNum(numArr);
-		return (numArr.length % 2 == 0) ? (numArr[(numArr.length/2)-1] + numArr[(numArr.length/2)]) / 2 : numArr[Math.floor(numArr.length/2)];
+        return (numArr.length % 2 == 0) ? (numArr[(numArr.length / 2) - 1] + numArr[(numArr.length / 2)]) / 2 : numArr[Math.floor(numArr.length / 2)];
     };
 
     // Quartile
-    Basic.quartile = function(aArrOfNum) {
+    Basic.quartile = function (aArrOfNum) {
         var numArr = aArrOfNum;
         numArr = Basic.sortNum(numArr);
-		
-		var q1 = Basic.median((numArr.length % 2 == 0) ? numArr.slice(0, (numArr.length / 2)) : numArr.slice(0, Math.floor(numArr.length / 2))),
-		q2 = Basic.median(numArr),
-		q3 = Basic.median((numArr.length % 2 == 0) ? numArr.slice((numArr.length / 2), numArr.length) : numArr.slice(Math.ceil(numArr.length / 2), numArr.length));
-		
-		return {
-			"lowVal" : numArr[0],                 // Lowest Value
-			"q1": q1,                             // Q1 - lower quartile
-			"q2": q2,                             // Q2 - middle quartile (median)
-			"q3": q3,                             // Q3 - upper quartile
-			"highval": numArr[numArr.length-1],   // highest value
-			"qi": q3 - q1                         // interquartile (Q3 - Q1)
-		};
+
+        var q1 = Basic.median((numArr.length % 2 == 0) ? numArr.slice(0, (numArr.length / 2)) : numArr.slice(0, Math.floor(numArr.length / 2))),
+            q2 = Basic.median(numArr),
+            q3 = Basic.median((numArr.length % 2 == 0) ? numArr.slice((numArr.length / 2), numArr.length) : numArr.slice(Math.ceil(numArr.length / 2), numArr.length));
+
+        return {
+            "lowVal": numArr[0],                    // Lowest Value
+            "q1": q1,                               // Q1 - lower quartile
+            "q2": q2,                               // Q2 - middle quartile (median)
+            "q3": q3,                               // Q3 - upper quartile
+            "highval": numArr[numArr.length - 1],   // highest value
+            "qi": q3 - q1                           // interquartile (Q3 - Q1)
+        };
     };
 
     // Generate Random Numbers from Min to Max Values
-    Basic.genRandom = function(aMinNum, aMaxNum) {
+    Basic.genRandom = function (aMinNum, aMaxNum) {
         var min = Math.ceil(aMinNum),
             max = Math.floor(aMaxNum);
         return Math.floor(Math.random() * (max - min + 1)) + min;
     };
 
     // Get Time
-    Basic.getTime = function(aBoolean) {
+    Basic.getTime = function (aBoolean) {
         var periods = aBoolean,
-        date = new Date(),
-		hh = date.getHours(),
-		mm = date.getMinutes(),
-        ss = date.getSeconds(),
-		pp = (hh < 12) ? "AM" : "PM";
-        
+            date = new Date(),
+            hh = date.getHours(),
+            mm = date.getMinutes(),
+            ss = date.getSeconds(),
+            pp = (hh < 12) ? "AM" : "PM";
+
         if (periods === true) hh = (hh > 12) ? hh - 12 : ((hh == 0) ? 12 : hh);
-		
-		if (ss < 10) ss = "0" + ss;
-		if (mm < 10) mm = "0" + mm;
-		if (hh < 10) hh = "0" + hh;
-		
-		ss = ss.toString();
-		mm = mm.toString();
-		hh = hh.toString();
-		
-		return (periods == true) ? {hh: hh, mm: mm, ss: ss, pp: pp} : {hh: hh, mm: mm, ss: ss};
+
+        if (ss < 10) ss = "0" + ss;
+        if (mm < 10) mm = "0" + mm;
+        if (hh < 10) hh = "0" + hh;
+
+        ss = ss.toString();
+        mm = mm.toString();
+        hh = hh.toString();
+
+        return (periods == true) ? { hh: hh, mm: mm, ss: ss, pp: pp } : { hh: hh, mm: mm, ss: ss };
     };
 
     // Convert String to URL Friendly
-    Basic.urlFriendly = function(aString, aCaseSense) {
+    Basic.urlFriendly = function (aString, aCaseSense) {
         var str = aString; // Input: String
-        var caseSense = (aCaseSense === true) ? true : false; // CaseSense: Boolean
-        
-        str = str.replace(/[^a-zA-Z -]/g, "");  // Special Characters Removed Except Hypens
-        str = str.replace(/\s/g, "-");          // Replace Spaces with Hypens
-        str = str.replace(/-+/g, "-");          // Consequtive Hypens Removed
-        str = str.replace(/^-+|-+$/g, "");      // Hypens in Begining and End was Removed
+        var caseSense = (aCaseSense === true) ? true : false;   // CaseSense: Boolean
+
+        str = str.replace(/[^a-zA-Z0-9 -]/g, "");               // Special Characters Removed Except Hypens
+        str = str.replace(/\s/g, "-");                          // Replace Spaces with Hypens
+        str = str.replace(/-+/g, "-");                          // Consequtive Hypens Removed
+        str = str.replace(/^-+|-+$/g, "");                      // Hypens in Begining and End was Removed
 
         return (caseSense) ? str : str.toLowerCase();
     };
-    
+
+    // Return Object
     return Basic;
 
 });
